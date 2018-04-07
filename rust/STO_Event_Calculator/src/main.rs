98e04af152731973e5c3a6f05dc8add3d79801cd
Andrew Nelson <andrew@andrewnelson.org>
2018-04-06 19:41:01 -0700

    Initial commit of the rust version.

---
+++ rust/STO_Event_Calculator/src/main.rs
@@ -0,0 +1,115 @@
+extern crate chrono;
+extern crate clap;
+extern crate time;
+
+mod input;
+
+use chrono::prelude::*;
+use clap::{Arg, App};
+use time::Duration;
+
+struct Times {
+    end: NaiveDate,
+    reset_hours: f64,
+    current_dt: chrono::NaiveDateTime,
+    current_date: chrono::NaiveDate,
+    days_needed: chrono::Duration
+    }
+
+impl Times {
+    fn reset(&self) -> chrono::NaiveDateTime {
+        self.current_dt + Duration::hours(self.reset_hours as i64)
+    }
+
+    fn remaining(&self) -> chrono::Duration {
+        NaiveDate::signed_duration_since(self.end, self.current_date)
+    }
+
+    fn final_day(&self) -> chrono::NaiveDate {
+        self.end - self.days_needed
+    }
+}
+
+fn main() {
+    let input_number = input::input_number;
+
+    const DATE_STR: &'static str = "%B %d %Y";
+    //const MIN_DATE_STR: &'static str = "%B %d";
+    const RESET_STR: &'static str = "%B %d %Y at %I:%M %p";
+    const DATE_FORMAT: &'static str = "%m/%d/%Y";
+
+    let args = App::new("STO Event Calendar")
+                        .version("0.1.0")
+                        .author("Andrew Nelson <evildarkarchon@gmail.com>")
+                        .about("Calculates if you have a chance to complete a Star Trek Online event and how long it will take.")
+                        .arg(Arg::with_name("daily")
+                            .short("d")
+                            .long("daily-tokens")
+                            .value_name("DAILY")
+                            .takes_value(true)
+                            .required(true)
+                            .help("Number of tokens awarded per day."))
+                        .arg(Arg::with_name("total")
+                            .short("t")
+                            .long("total-tokens")
+                            .value_name("TOTAL")
+                            .takes_value(true)
+                            .required(true)
+                            .help("Total number of tokens require to complete the event."))
+                        .arg(Arg::with_name("claimed")
+                            .short("c")
+                            .long("tokens-claimed")
+                            .value_name("TOKENS")
+                            .takes_value(true)
+                            .required(true)
+                            .help("Number of tokens claimed so far."))
+                        .arg(Arg::with_name("end")
+                            .short("e")
+                            .long("end-date")
+                            .value_name("END")
+                            .takes_value(true)
+                            .required(true)
+                            .help("Date that the event ends (in MM/DD/YYYY format)."))
+                        .arg(Arg::with_name("reset")
+                            .short("r")
+                            .long("reset")
+                            .takes_value(true)
+                            .required(true)
+                            .value_name("RESET")
+                            .help("Number of hours until the daily quest(s) reset."))
+                        .get_matches();
+
+    let claimed = input_number(&args, "claimed");
+    let total = input_number(&args, "total");
+    let daily = input_number(&args, "daily");
+
+    let times = Times {
+        end: NaiveDate::parse_from_str(args.value_of("end").unwrap(), DATE_FORMAT)
+                                                                                .ok()
+                                                                                .unwrap(),
+        reset_hours: input_number(&args, "reset"),
+        current_dt: Utc::now().naive_utc(),
+        current_date: Utc::now().naive_utc().date(),
+        days_needed: Duration::days((total - claimed) as i64 / daily as i64)
+    };
+
+    let reset = times.reset();
+
+    let remaining = times.remaining();
+
+    let final_day = times.final_day();
+
+    assert!(times.days_needed.num_days() as i64 > 0, "times.days_needed returned a negative number or is 0. times.days_needed value is: {}", times.days_needed.num_days() as i64);
+    assert!(remaining.num_days() > 0, "remaining returned a negative number or is 0. remaining value is: {}", remaining.num_days());
+    assert!(times.reset_hours as i64 > 0, "Reset time must be a positive number.");
+
+    println!("Today's Date is {}", times.current_date.format(DATE_STR));
+    println!("Days remaining in event is: {}", remaining.num_days());
+    println!("Daily quests reset at approximately: {}", reset.format(RESET_STR));
+
+    if final_day == times.current_date {
+        println!("You must start the event today to acquire the required number of tokens.");
+    } else {
+        println!("Final day to start the event is: {}", final_day.format(DATE_STR));
+    }
+}
