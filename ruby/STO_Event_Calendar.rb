64e42efb60d51eebe881005e666013eabce664a7
Andrew Nelson <andrew@andrewnelson.org>
2018-06-21 20:58:31 -0700

    Switch from Date to Time

--- ruby/STO_Event_Calendar.rb
+++ ruby/STO_Event_Calendar.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require date
+require time
 require optparse
 # rubocop:disable Layout/SpaceAroundOperators
 require optparse/date
@@ -20,17 +20,17 @@ OptionParser.new do |parser|
   parser.on('-d', '--daily [DAILY]', Integer, 'Amount of tokens acquired per day.') { |d| stats.daily = d }
   parser.on('-n', '--needed [NEEDED]', Integer, 'Number of tokens needed to complete event.') { |n| stats.needed = n }
   parser.on('-c', '--claimed [CLAIMED]', Integer, 'Number of tokens already claimed.') { |c| stats.claimed = c }
-  parser.on('-e', '--end [END]', Date, 'End date of the event.') { |e| stats.end = e }
+  parser.on('-e', '--end [END]', Time, 'End date of the event.') { |e| stats.end = e }
   parser.on('-r', '--reset [RESET]', Float, 'Amount of hours until dailies reset.') { |r| stats.reset = r }
 end
 # rubocop:enable Metrics/LineLength
 
-today = date.now
+today = Time.now
 
 # rubocop:disable Metrics/LineLength, Style/StringLiterals
 stats.daily = gatherinfo('Max tokens per day: ').to_i unless stats.daily.respond_to?(:to_i)
 stats.needed = gatherinfo('Total tokens needed: ').to_i unless stats.needed.respond_to?(:to_i)
 stats.claimed = gatherinfo('Tokens already claimed: ').to_i unless stats.claimed.respond_to?(:to_i)
-stats.end = Date.strptime(gatherinfo('End Date (MM/DD/YYYY): '), "%m/%d/%Y") unless stats.end.respond_to?(:to_date)
+stats.end = Time.strptime(gatherinfo('End Date (MM/DD/YYYY): '), "%m/%d/%Y") unless stats.end.respond_to?(:to_time)
 stats.reset = gatherinfo('Hours until reset: ').to_f unless stats.reset.respond_to?(:to_f)
 # rubocop:enable Metrics/LineLength, Style/StringLiterals
