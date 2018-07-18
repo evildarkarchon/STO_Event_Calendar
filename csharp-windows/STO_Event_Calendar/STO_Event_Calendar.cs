7549bd519483df9230b2772e529f4e0b4c9c5ef4
Andrew Nelson <andrew@andrewnelson.org>
2018-07-18 00:47:49 -0700

    Initial commit for C# version
    There (almost) no changes to the file, it looks like a hello world program with the start of an additional class.

---
+++ csharp-windows/STO_Event_Calendar/STO_Event_Calendar.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace STO_Event_Calendar
+{
+    class Options {
+      
+    }
+
+    class STO_Event_Calendar
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
