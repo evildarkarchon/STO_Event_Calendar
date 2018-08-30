015e291183ec884ef2202c33cbcf1b94a89f77a4
Andrew Nelson <andrew@andrewnelson.org>
2018-08-29 17:46:11 -0700

    Change name and return type of input_number to input_number_f32.
    Add a similar function that returns an i32 instead of an f32.

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,14 @@
 use clap::ArgMatches;
 
-pub fn input_number(cmd: &ArgMatches, cmdname: &str) -> f64 {
-    let intermediate: f64 = cmd.value_of(cmdname)
+pub fn input_number_f32(cmd: &ArgMatches, cmdname: &str) -> f32 {
+    let intermediate: f32 = cmd.value_of(cmdname)
+                                                .unwrap()
+                                                .parse()
+                                                .unwrap();
+        intermediate
+}
+pub fn input_number_i32(cmd: &ArgMatches, cmdname: &str) -> i32 {
+    let intermediate: i32 = cmd.value_of(cmdname)
                                                 .unwrap()
                                                 .parse()
                                                 .unwrap();
