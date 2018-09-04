f711054bc058cfac800e9d749c5285b281489c25
Andrew Nelson <andrew@andrewnelson.org>
2018-09-04 01:02:10 -0700

    Change input_number_i32 to input_number_u32

--- src/input.rs
+++ src/input.rs
@@ -1,16 +1,19 @@
 use clap::ArgMatches;
 
 pub fn input_number_f32(cmd: &ArgMatches, cmdname: &str) -> f32 {
-    let intermediate: f32 = cmd.value_of(cmdname)
-                                                .unwrap()
-                                                .parse()
-                                                .unwrap();
+    let intermediate: f32 = cmd.value_of(cmdname).unwrap().parse().ok().unwrap();
+    assert!(intermediate > 0 as f32, "You must put in a value greater than 0");
         intermediate
 }
-pub fn input_number_i32(cmd: &ArgMatches, cmdname: &str) -> i32 {
-    let intermediate: i32 = cmd.value_of(cmdname)
-                                                .unwrap()
-                                                .parse()
-                                                .unwrap();
+
+pub fn input_number_u32(cmd: &ArgMatches, cmdname: &str) -> u32 {
+    let intermediate: u32 = cmd.value_of(cmdname).unwrap().parse().ok().unwrap();
+    assert!(intermediate > 0 as u32, "You must put in a value greater than 0");
         intermediate
+}
+
+pub fn ask(message: &str) -> String {
+    print!("{}", message);
+    let out: String = read!("{}\n");
+    out
 }
