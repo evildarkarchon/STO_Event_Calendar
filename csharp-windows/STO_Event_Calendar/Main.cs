3d5bc2623012be631a586eca8e171d1230a67aa5
Andrew Nelson <evildarkarchon@gmail.com>
2018-10-15 19:32:58 -0700

    Main.cs is now the main .cs file

---
+++ csharp-windows/STO_Event_Calendar/Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace STO_Event_Calendar
+{
+    public struct Date
+    {
+        public string EndDate;
+        public float Reset;
+        public uint Needed;
+        public uint Tokens;
+        public uint Daily;
+    }
+
+    class STO_Event_Calendar
+    {
+        public static void Main(string[] args)
+        {
+            ParserResult<Options> result = Parser.Default.ParseArguments<Options>(args);
+            STOFactory Factory = new STOFactory();
+            Date Dates = default(Date);
+            STO DateCalc = default(STO);
+            bool UseOptions = default(bool);
+            Options Opts = default(Options);
+
+            if (result.Tag == ParserResultType.Parsed)
+            {
+                Opts = ((Parsed<Options>)result).Value;
+            }
+
+            if (result.Tag == ParserResultType.Parsed &&
+                Opts.DailyTokens == default(uint) &&
+                (string.IsNullOrEmpty(Opts.EndDate) || Opts.EndDate == default(string)) &&
+                Opts.Reset == default(float) &&
+                Opts.TokensClaimed == default(uint) &&
+                Opts.TotalTokens == default(uint))
+            {
+                UseOptions = false;
+            }
+            else if (result.Tag == ParserResultType.Parsed)
+            {
+                UseOptions = true;
+            }
+
+            if (UseOptions == true)
+            {
+                DateCalc = Factory.Create(ref Opts);
+            }
+            else if (UseOptions == false)
+            {
+                DateCalc = Factory.Create(ref Dates);
+            }
+        }
+    }
+}
