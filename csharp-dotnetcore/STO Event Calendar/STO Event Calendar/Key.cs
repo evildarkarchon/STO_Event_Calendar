05df1fe5462a31a93991299268a3f04b70598d16
Andrew Nelson <evildarkarchon@gmail.com>
2018-12-28 21:22:25 -0800

    Add Key.cs, which contains the keyboard input convenience function.

---
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Key.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Keyboard
+{
+    class Key
+    {
+        public static string Ask(string message, string exceptionmsg = default(string), bool newline = false, bool is_required = true)
+        {
+            if (newline == true) { Console.WriteLine(message); }
+            else { Console.Write(message); }
+
+            string Output = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(Output) || Output == default(string))
+            {
+                if (is_required && exceptionmsg != default(string) && !string.IsNullOrEmpty(exceptionmsg)) { throw new ArgumentException(exceptionmsg); }
+                else if (is_required && (exceptionmsg == default(string) || string.IsNullOrEmpty(exceptionmsg))) { throw new ArgumentException(exceptionmsg); }
+            }
+
+            return Output;
+        }
+    }
+}
