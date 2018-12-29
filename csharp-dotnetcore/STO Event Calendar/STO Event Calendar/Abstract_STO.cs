044bf4c75237f2516717c2f5b14ae52b7ecbce3c
Andrew Nelson <evildarkarchon@gmail.com>
2018-12-28 19:32:52 -0800

    Initial commit for Abstract_STO.cs

---
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Abstract_STO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STO_Event_Calendar
+{
+    public abstract class AbsSTO
+    {
+        public Date Dates { get; protected set; }
+
+        public DateTime Reset { get; protected set; }
+        
+    }
+}
