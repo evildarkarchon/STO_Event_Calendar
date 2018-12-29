a270400961d187688f4cec2d68c1d2914739b57b
Andrew Nelson <evildarkarchon@gmail.com>
2018-12-28 17:00:26 -0800

    Initial commit of csharp-dotnetcore

---
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace STO_Event_Calendar
+{
+    public struct Date
+    {
+        public string EndDate;
+        public float Reset;
+        public uint Needed;
+        public uint Tokens;
+        public uint Daily;
+    }
+
+    class STO
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
