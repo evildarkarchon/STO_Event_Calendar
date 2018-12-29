b34a2da164f788d8af792bbbb4654dd73d05408b
Andrew Nelson <evildarkarchon@gmail.com>
2018-12-28 21:21:32 -0800

    Add Date.cs, which contains the Date struct.

---
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Date.cs
@@ -0,0 +1,11 @@
+﻿namespace STO_Event_Calendar
+{
+    public struct Date
+    {
+        public string EndDate;
+        public float Reset;
+        public uint Needed;
+        public uint Tokens;
+        public uint Daily;
+    }
+}
