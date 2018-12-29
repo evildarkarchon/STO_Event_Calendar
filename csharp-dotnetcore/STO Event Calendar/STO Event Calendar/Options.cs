7833c179f32d062e9123a221337cbd9b9007eed8
Andrew Nelson <evildarkarchon@gmail.com>
2018-12-28 17:13:42 -0800

    Initial commit for Options.cs

---
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Options.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using CommandLine;
+namespace STO_Event_Calendar
+{
+    public class Options
+    {
+        [Option('d', "daily-tokens", HelpText = "Amount of tokens you get per reset.")]
+        public uint DailyTokens { get; set; }
+
+    }
+}
+
