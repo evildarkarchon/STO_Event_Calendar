cfe14a023fb4cba1d9b564d23432e77ca2f7afc3
Andrew Nelson <evildarkarchon@gmail.com>
2019-01-03 20:16:36 -0800

    Trying to use result.MapResult instead of Tag checking hacks.

--- csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Main.cs
+++ csharp-dotnetcore/STO Event Calendar/STO Event Calendar/Main.cs
@@ -13,23 +13,41 @@ namespace STO_Event_Calendar
             DateTime End = DateTime.Now + TimeSpan.FromDays(20);
             STO DateCalc = Factory.Create(End.ToString(), 20.0f, 20u, 20u, 10u);
             bool UseOptions = new bool();
-            Options Opts = new Options();
-            /*
-            result.WithParsed(options => {
-                if (options.Quiet && (!options.Json || !options.PrintJSON)) {
-                    options.PrintJSON = true;
-                    Console.WriteLine("Neither --json or --print-json were specified, activating --print-json");
-                }
-            });
-            */ // Can't set outside variables within a lambda (if I remember correctly)
+            //Options Opts = new Options();
 
+            /*
             if (result.Tag == ParserResultType.Parsed) {
-                if (Opts.Quiet && !Opts.Json || !Opts.PrintJSON) {
+                if (Opts.Quiet == true && Opts.Json == false && Opts.PrintJSON == false) {
                     Opts.PrintJSON = true;
                     Console.Error.WriteLine("Neither --json or --print-json were specified, activating --print-json\n");
                 }
             }
+            */
 
+            result.WithParsed(options =>
+           {
+               if (options.Quiet == true && options.Json == false && options.PrintJSON == false)
+               {
+                   options.PrintJSON = true;
+                   Console.Error.WriteLine("Neither --json or --print-json were specified, activating --print-json\n");
+               }
+           });
+
+            UseOptions = result.MapResult(options => {
+                if (options.DailyTokens == default(uint) && 
+                (options.EndDate == default(string) || string.IsNullOrEmpty(options.EndDate)) && 
+                options.Reset == default(float) &&
+                options.TokensClaimed == default(uint) &&
+                options.TotalTokens == default(uint)) { return false; }
+                else { return true; }
+            }, _ => { return false; }
+             );
+
+            DateCalc = result.MapResult(options => { if (UseOptions == true) { return Factory.Create(ref options); }
+                else { return Factory.Create(ref Dates, ref options); } }, 
+                _ => { return DateCalc; });
+
+            /*
             if (result.Tag == ParserResultType.Parsed)
             {
                 Opts = ((Parsed<Options>)result).Value;
@@ -57,6 +75,7 @@ namespace STO_Event_Calendar
             {
                 DateCalc = Factory.Create(ref Dates, ref Opts);
             }
+            */
 
             if (DateCalc != default(STO))
             {
@@ -75,6 +94,7 @@ namespace STO_Event_Calendar
                 }
                 );
             }
+            
         }
     }
 }
